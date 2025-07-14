27aaa30b42d84e96992dcc6ef09248dd7d585c62
Anamitra Upadhyay <anamitraupadhyay@gmail.com>
2025-07-14 14:38:19 +0530

    added the final orchestrated main.c outside of any directories of be it vmstat and slabinfo

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("now the need is to orchestrate the files from vmstat and slabinfo and collectively calculate the memory leak taking place and the logic is to be designed further");
+    return 0;
+}
